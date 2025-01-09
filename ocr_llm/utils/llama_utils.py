0dc0fce40fdda4ed2c3274e9f21fcd45dac62cca
Bilal Motassim <95474983+Bilal-Motassim@users.noreply.github.com>
2025-01-09 17:31:18 +0100

    Update llama_utils.py

--- ocr_llm/utils/llama_utils.py
+++ ocr_llm/utils/llama_utils.py
@@ -14,7 +14,7 @@ def clean_ocr_text(raw_text: str) -> str:
     return "\n".join(cleaned_lines)
 
 
-def send_prompt_to_llm(prompt, model="llama2", endpoint="https://hz07pb0wau1neo4a.us-east-1.aws.endpoints.huggingface.cloud/v1/chat/completions"):
+def send_prompt_to_llm(prompt, model="llama2", endpoint="https://d67gycs2panm5pfz.us-east-1.aws.endpoints.huggingface.cloud/v1/chat/completions"):
     try:
         response = requests.post(
             endpoint,
